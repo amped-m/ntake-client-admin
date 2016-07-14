c87a9deab7596d8b0ed31a98992f5f243e7930f9
eweba1-pc-37 <teamlead@a1professionals.com>
2016-07-14 18:24:02 +0530

    show call details using twilio api

--- view/client.php
+++ view/client.php
@@ -1,22 +1,3 @@
-<?php
-// Get the PHP helper library from twilio.com/docs/php/install
-require('lib/Services/Twilio.php'); // Loads the library
-
-// Your Account Sid and Auth Token from twilio.com/user/account
-$account_sid = "ACe46cfc552dcd97c22f3c2a3ae448ebb6"; // Your Twilio account sid
-$auth_token = "b846d4a2a3dbdb309cc284788904769e"; // 
-$client = new Services_Twilio($account_sid, $auth_token);
-
-$numbers = $client->account->available_phone_numbers->getList('US', 'Local', array(
-        "AreaCode" => "510"
-    ));
-foreach($numbers->available_phone_numbers as $number) {
-	echo"<pre>";
-echo $number->phone_number;
-echo"</pre>";
-}
-
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
@@ -41,7 +22,7 @@ echo"</pre>";
         $("#verification_code").text(code);
         $("#verify_code").fadeIn();
         $("#enter_number").fadeOut();
-      }a
+      }
 
       function checkStatus() {
         $.post("status.php", { phone_number : $("#phone_number").val() }, 
@@ -62,8 +43,51 @@ echo"</pre>";
         $("#status").text("Verified!");
       }
     </script>
+    
+    <style>
+		.table-nm tr {line-height: 30px;}
+		.table-nm td, th {padding: 0 10px;}
+    </style>
   </head>
   <body>
+	  <?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('lib/Services/Twilio.php'); // Loads the library
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "AC1fa65a81c2591b246215920ca9ffc9fd"; 
+$token = "300c4269a673067451610fa823b7030a";
+$client = new Services_Twilio($sid, $token);
+
+// Get an object from its sid. If you do not have a sid,
+// check out the list resource examples on this page
+
+?>
+<table class="table-nm">
+	<tr>
+		<th>Phone Number</th>
+		<th>Call Status</th>
+		<th>Call Start Time</th>
+		<th>Call End Time</th>
+		<th>Call Duration</th>
+		<th>Call Type</th>
+		</tr>
+<?php
+foreach ($client->account->calls as $call) 
+{
+	?>
+	<tr>
+		<td><?php echo $call->to; ?></td>
+		<td><?php echo $call->status; ?></td>
+		<td><?php echo $call->start_time; ?></td>
+		<td><?php echo $call->end_time; ?></td>
+		<td><?php echo $call->duration; ?></td>
+		<td><?php echo $call->direction; ?></td>
+	</tr>
+	<?php
+} 
+	  ?>
+	  </table>
     <form id="enter_number">
       <p>Enter your phone number:</p>
       <p><input type="text" name="phone_number" id="phone_number" /></p>
@@ -76,5 +100,6 @@ echo"</pre>";
       <h1 id="verification_code"></h1>
       <p><strong id="status">Waiting...</strong></p>
     </div>
+    
   </body>
 </html>
